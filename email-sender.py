9472afecc4f98c16eac75311dc98d121a712305d
Alberto Tomasin <alberto.tomasin@gmail.com>
2022-10-08 11:51:34 +0200

    update
    aggiunte variablili d'ambiente e
    
    Took 7 hours 25 minutes

--- email-sender.py
+++ email-sender.py
@@ -3,23 +3,24 @@ nel caso in cui venga lanciato un errore sul certificato ssl eseguire i seguenti
 - installare certifi
 - eseguire lo script install_certifi.py
 """
-
+from dotenv import load_dotenv
 from email.message import EmailMessage
+import os
 import ssl
 import smtplib
 import csv
 
+load_dotenv()
 # credenziali di chi invia la mail
-sender_mail = ""
-password = ""
+sender_mail = os.environ.get('EMAIL')
+password = os.environ.get('PASSWORD')
 
 # oggetto e corpo della mai
-subject = ""
-body = ""
-
+subject = "formazione python"
+body = "grazie per aver seguito la formazione"
 
 context = ssl.create_default_context()
-with open("test-email.csv") as file:
+with open('test-email.csv') as file:
     reader = csv.reader(file)
     next(reader)
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
